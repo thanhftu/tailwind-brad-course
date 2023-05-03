c779aac18f100e0bb2e37c94578a3a0b5d5cc16d
MacBook Pro <macbookpro@MacBooks-MBP.lan>
2023-05-03 23:31:00 +0700

    add features

---
+++ src/components/Features.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+
+function Features() {
+  return (
+    <section id="features">
+      <div className="container flex flex-col mx-auto px-4 mt-10 space-y-12 md:space-y-0 md:flex-row">
+        {/* What's Diffences */}
+        <div className="flex flex-col space-y-12 md:w-1/2">
+          <h1 className="text-4xl max-w-md font-bold text-center md:text-left">
+            {" "}
+            What's different about Manage?
+          </h1>
+          <p className="text-center md:text-left max-w-sm text-darkGrayishBlue">
+            Manage provides all the functionality your team needs, without the
+            complexity. Our software is tailor-made for modern digital product
+            teams.
+          </p>
+        </div>
+
+        {/* Number List */}
+        <div className="flex flex-col space-y-8 md:w-1/2">
+          {/* List1 */}
+          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+            {/* heading */}
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
+                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                  01
+                </div>
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
+                  Track Company-wide Progress
+                </h3>
+              </div>
+            </div>
+            <div>
+              <h3 className="hidden text-base font-bold md:mb-4 md:block">
+                Track Company-wide Progress
+              </h3>
+              <p className="text-darkGrayishBlue">
+                See how your day-to-day tasks fit into the wider vision. Go from
+                tracking progress at the milestone level all the way down to the
+                smallest of details. Never lose sight of the bigger picture
+                again.
+              </p>
+            </div>
+          </div>
+
+          {/* List2 */}
+          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+            {/* heading */}
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
+                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                  02
+                </div>
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
+                  Advanced built-in reports
+                </h3>
+              </div>
+            </div>
+            <div>
+              <h3 className="hidden text-base font-bold md:mb-4 md:block">
+                Advanced built-in reports
+              </h3>
+              <p className="text-darkGrayishBlue">
+                Set internal delivery estimates and track progress toward
+                company goals. Our customisable dashboard helps you build out
+                the reports you need to keep key stakeholders informed.
+              </p>
+            </div>
+          </div>
+          {/* List1 */}
+          <div className="flex flex-col space-y-3 md:space-y-0 md:space-x-6 md:flex-row">
+            {/* heading */}
+            <div className="rounded-l-full bg-brightRedSupLight md:bg-transparent">
+              <div className="flex items-center space-x-2">
+                <div className="px-4 py-2 text-white rounded-full md:py-1 bg-brightRed">
+                  03
+                </div>
+                <h3 className="text-base font-bold md:mb-4 md:hidden">
+                  Everything you need in one place
+                </h3>
+              </div>
+            </div>
+            <div>
+              <h3 className="hidden text-base font-bold md:mb-4 md:block">
+                Everything you need in one place
+              </h3>
+              <p className="text-darkGrayishBlue">
+                Stop jumping from one service to another to communicate, store
+                files, track tasks and share documents. Manage offers an
+                all-in-one team productivity solution.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
